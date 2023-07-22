3b5f04cf4819e49d2015d69c84b911d2b3983670
Mateo Vasco <macv9620@utp.edu.co>
2023-07-21 21:53:05 -0500

    Se migra get products a express y postgreslq

--- src/services/useGetProductsApi.jsx
+++ src/services/useGetProductsApi.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "https://get-yours.onrender.com/items"
-
 const useGetProductsApi = (setRenderLoadingSpinner, setRenderErrorPage) => {
   const [products, setProducts] = useState(null);
   const [updateProducts, setUpdateProducts] = useState(true)
   useEffect(() => {
+
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:9000/items',
+      headers: { }
+    };
+
     if(updateProducts){
       setRenderLoadingSpinner(true);
-      axios.get(BASE_URL)
+      axios.request(config)
         .then((products) => {
+          console.log('Request Get Produts')
+          console.log(products)
           setRenderLoadingSpinner(false);
           setProducts(products.data)
           setUpdateProducts(null)
