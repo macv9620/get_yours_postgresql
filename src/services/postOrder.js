e6e2a0a6d803de7a58b51703b54b3e3f5c579daf
Mateo Vasco <macv9620@utp.edu.co>
2023-07-22 23:07:59 -0500

    Se crea endpoint para enviar órdenes

---
+++ src/services/postOrder.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+const BASE_URL_ORDER = "http://localhost:9000/orders";
+const BASE_URL_ORDER_PRODUCT = "http://localhost:9000/orders/products";
+
+const postOrder = (data, token) => {
+
+  let config = {
+    method: "delete",
+    maxBodyLength: Infinity,
+    url: BASE_URL_ORDER,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    data: data,
+  };
+
+  return axios.request(config);
+};
+
+const postOrderProducts = (data, token) => {
+
+    let config = {
+      method: "delete",
+      maxBodyLength: Infinity,
+      url: BASE_URL_ORDER_PRODUCT,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: data,
+    };
+  
+    return axios.request(config);
+  };
+
+
+
+export { postOrderProducts, postOrder };
